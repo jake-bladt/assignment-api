6a276af2431ed70e8430c021d770bdf1e37207f1
Jake Bladt <jake@jakebladt.com>
2018-07-26 01:12:13 -0400

    +viewmodel

---
+++ JakeBladt.AssignmentAPI/ViewModels/AssignmentViewmodel.cs
@@ -0,0 +1,29 @@
+﻿using JakeBladt.AssignmentAPI.Domain;
+
+namespace JakeBladt.AssignmentAPI.ViewModels
+{
+    public class AssignmentViewmodel : Assignment
+    {
+        public string Self { get; set; }
+        public string[] Tags { get; set; }
+
+        private static string SelfLink(int id)
+        {
+            return $"/api/v0.1/assignments/{id}";
+        }
+
+        public static AssignmentViewmodel FromAssignment(Assignment assignment)
+        {
+            return new AssignmentViewmodel
+            {
+                AssignmentId = assignment.AssignmentId,
+                Name = assignment.Name,
+                Description = assignment.Description,
+                Title = assignment.Title,
+                AssignmentType = assignment.AssignmentType,
+                Duration = assignment.Duration,
+                Self = SelfLink(assignment.AssignmentId)
+            };
+        }
+    }
+}
