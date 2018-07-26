dcf536983860fbad04b801cf23c4071b76125114
Jake Bladt <jake@jakebladt.com>
2018-07-26 01:07:24 -0400

    +Program/Startup

--- JakeBladt.AssignmentAPI/Program.cs
+++ JakeBladt.AssignmentAPI/Program.cs
@@ -1,25 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+
+using System.IO;
 
 namespace JakeBladt.AssignmentAPI
 {
     public class Program
     {
+        public static IConfigurationRoot Configuration;
+        public static string DBConnectionString;
+
         public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            DBConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+        }
     }
 }
--- JakeBladt.AssignmentAPI/Startup.cs
+++ JakeBladt.AssignmentAPI/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+
+using JakeBladt.AssignmentAPI.Data;
 
 namespace JakeBladt.AssignmentAPI
 {
@@ -24,6 +20,7 @@ namespace JakeBladt.AssignmentAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApiDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
