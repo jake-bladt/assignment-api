5c5f7f1a9421b5617156e11f2d9921229141cec4
Jake Bladt <jake@jakebladt.com>
2018-07-26 01:21:19 -0400

    +controller

---
+++ JakeBladt.AssignmentAPI/Controllers/AssignmentsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+using JakeBladt.AssignmentAPI.Data;
+using JakeBladt.AssignmentAPI.Domain;
+using JakeBladt.AssignmentAPI.ViewModels;
+
+namespace JakeBladt.AssignmentAPI.Controllers
+{
+    [Route("api/v0.1/[controller]")]
+    public class AssignmentsController : Controller
+    {
+        protected ApiDbContext _context;
+
+        public AssignmentsController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string tag)
+        {
+            // The only requirement for this endpoint is that it be able to get multiple assignments by
+            // tag. All non-tag queries respond with a 404.
+            if (String.IsNullOrEmpty(tag)) return NotFound();
+
+            try
+            {
+                var assignments = from a in _context.AssignmentsByTag(tag) select AssignmentViewmodel.FromAssignment(a);
+                return Ok(assignments);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var assignment = _context.Assignments.FirstOrDefault(a => a.AssignmentId == id);
+            if (null == assignment)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var vm = AssignmentViewmodel.FromAssignment(assignment);
+                return Ok(vm);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(string name, string assignmentType, string title, string description, string duration, string tags)
+        {
+            var tagList = string.IsNullOrEmpty(tags) ? new string[0] : tags.Split(",");
+
+            var newAssignment = new Assignment
+            {
+                Name = name,
+                AssignmentType = assignmentType,
+                Title = title,
+                Description = description,
+                Duration = duration
+            };
+
+            try
+            {
+                _context.Assignments.Add(newAssignment);
+                _context.SaveChanges();
+
+                if (tagList.Length > 0)
+                {
+                    for (int i = 0; i < tagList.Length; i++)
+                    {
+                        var tag = _context.Tags.FirstOrDefault(t => t.Name == tagList[i]);
+                        if (null == tag)
+                        {
+                            tag = new Tag { Name = tagList[i] };
+                            _context.Tags.Add(tag);
+                            _context.SaveChanges();
+                        }
+
+                        var link = new AssignmentTag
+                        {
+                            Assignment = newAssignment,
+                            AssignmentId = newAssignment.AssignmentId,
+                            Tag = tag,
+                            TagId = tag.TagId
+                        };
+                        newAssignment.AssignmentTags.Add(link);
+                    }
+                }
+
+                _context.SaveChanges();
+                var vm = AssignmentViewmodel.FromAssignment(newAssignment);
+                vm.Tags = tagList;
+                return Ok(vm);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+                return StatusCode(500);
+            }
+
+        }
+    }
+}
